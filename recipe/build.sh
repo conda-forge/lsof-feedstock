fd40f755a7d09885f592083dcb2485b0d9b0884d
Marcel Bargull <marcel.bargull@udo.edu>
2021-10-07 16:50:22 +0200

    Use compiler env vars

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-./Configure -n $(uname|tr '[:upper:]' '[:lower:]')
+LSOF_CC="${CC} ${CPPFLAGS} ${CFLAGS} ${LDFLAGS}" \
+    ./Configure -n $(uname|tr '[:upper:]' '[:lower:]')
 make -j${CPU_COUNT}
 mkdir -p $PREFIX/bin/
 cp ./lsof $PREFIX/bin/
